427436ef36a12eea86c9e77a36f224af2b87f7e4
Christopher Neal <wandadars@gmail.com>
2025-12-20 04:23:18 -0500

    adding comments to standard grid reader functions

--- src/include/LociGridReaders.h
+++ src/include/LociGridReaders.h
@@ -26,20 +26,21 @@
 
 namespace Loci {
 
-  //Define struct Area which data members of normal vector and area of the area
+  /// Define struct Area which data members of normal vector and area of the area
   template <class REAL> struct Area_t {
     vector3d<REAL> n ;  //normal vector of the face
     REAL sada ; //area of the face
   } ;
 
-  //Overload ostream and istream (Input/Output) operators for struct Area
-  template<class REAL> 
+  /// Overload ostream (output) operator for struct Area
+  template<class REAL>
   inline std::ostream & operator<<(std::ostream &s, const Area_t<REAL> &v)
   {
     s << v.n << ' ' << v.sada << ' ' ;
     return s ;
   }
 
+  /// Overload istream (input) operator for struct Area
   template <class REAL>
   inline std::istream &operator>>(std::istream &s, Area_t<REAL> &v)
   {
@@ -58,8 +59,10 @@ namespace Loci {
       return DatatypeP(ct) ;
     }
   } ;
-  
-  
+
+
+  /// Defines a rigid body transformation consisting of a rotation about an
+  /// axis and a translation: v' = R * (v + t1) + t2
   struct rigid_transform {
     vector3d<double> t1,t2 ;
     tensor3d<double> R,Rinv ;
@@ -105,11 +108,11 @@ namespace Loci {
       tRinv.x.x = t.x.x*Rinv.x.x+t.x.y*Rinv.y.x+t.x.z*Rinv.z.x ;
       tRinv.y.x = t.y.x*Rinv.x.x+t.y.y*Rinv.y.x+t.y.z*Rinv.z.x ;
       tRinv.z.x = t.z.x*Rinv.x.x+t.z.y*Rinv.y.x+t.z.z*Rinv.z.x ;
-      
+
       tRinv.x.y = t.x.x*Rinv.x.y+t.x.y*Rinv.y.y+t.x.z*Rinv.z.y ;
       tRinv.y.y = t.y.x*Rinv.x.y+t.y.y*Rinv.y.y+t.y.z*Rinv.z.y ;
       tRinv.z.y = t.z.x*Rinv.x.y+t.z.y*Rinv.y.y+t.z.z*Rinv.z.y ;
-      
+
       tRinv.x.z = t.x.x*Rinv.x.z+t.x.y*Rinv.y.z+t.x.z*Rinv.z.z ;
       tRinv.y.z = t.y.x*Rinv.x.z+t.y.y*Rinv.y.z+t.y.z*Rinv.z.z ;
       tRinv.z.z = t.z.x*Rinv.x.z+t.z.y*Rinv.y.z+t.z.z*Rinv.z.z ;
@@ -118,11 +121,11 @@ namespace Loci {
       RtRinv.x.x = R.x.x*tRinv.x.x+R.x.y*tRinv.y.x+R.x.z*tRinv.z.x ;
       RtRinv.y.x = R.y.x*tRinv.x.x+R.y.y*tRinv.y.x+R.y.z*tRinv.z.x ;
       RtRinv.z.x = R.z.x*tRinv.x.x+R.z.y*tRinv.y.x+R.z.z*tRinv.z.x ;
-      
+
       RtRinv.x.y = R.x.x*tRinv.x.y+R.x.y*tRinv.y.y+R.x.z*tRinv.z.y ;
       RtRinv.y.y = R.y.x*tRinv.x.y+R.y.y*tRinv.y.y+R.y.z*tRinv.z.y ;
       RtRinv.z.y = R.z.x*tRinv.x.y+R.z.y*tRinv.y.y+R.z.z*tRinv.z.y ;
-      
+
       RtRinv.x.z = R.x.x*tRinv.x.z+R.x.y*tRinv.y.z+R.x.z*tRinv.z.z ;
       RtRinv.y.z = R.y.x*tRinv.x.z+R.y.y*tRinv.y.z+R.y.z*tRinv.z.z ;
       RtRinv.z.z = R.z.x*tRinv.x.z+R.z.y*tRinv.y.z+R.z.z*tRinv.z.z ;
@@ -136,7 +139,7 @@ namespace Loci {
       //      return vector3d<realT>(rotate_vec(vpt1)+t2 ;
     }
   } ;
-  
+
   struct periodic_info {
     std::string name ;
     bool master,processed ;
@@ -166,20 +169,55 @@ namespace Loci {
       LOCI_INSERT_TYPE(ct,Loci::rigid_transform,R) ;
       LOCI_INSERT_TYPE(ct,Loci::rigid_transform,Rinv) ;
       return DatatypeP(ct) ;
-      
+
     }
   } ;
 
+  /// Reads grid structures in the fact database
+  /// @param[in,out] facts Fact database to add grid-related facts into.
+  /// @param[in] filename Input grid filename.
+  /// @param[in] cellwts Optional cell weights for partitioning.
+  /// @return true if success
   bool readFVMGrid(fact_db &facts, std::string filename,storeRepP cellwts=0) ;
-  
+
+  /// Sets up FVM grid in the fact database
+  /// @param[in,out] facts Fact database to set up grid into.
+  /// @param[in] filename Input grid filename.
+  /// @param[in] cellwts Optional cell weights for partitioning.
+  /// @return true if success
   bool setupFVMGrid(fact_db &facts, std::string filename,storeRepP cellwts=0) ;
+
+  /// Sets up FVM grid in the fact database for the case where cell weights are
+  /// stored in a storeRep.
+  /// @param[in,out] facts Fact database to set up grid into.
+  /// @param[in] filename Input grid filename.
+  /// @param[in] cellwts  cell weights for partitioning.
+  /// @return true if success
   bool setupFVMGridWithWeightInStore(fact_db &facts, std::string filename, storeRepP cellwt );
+
+  /// Sets up FVM grid in the fact database for the case where cell weights are
+  /// stored in a file.
+  /// @param[in,out] facts Fact database to set up grid into.
+  /// @param[in] filename Input grid filename.
+  /// @param[in] weightfile  filename for file containing cell weights.
+  /// @return true if success
   bool setupFVMGridWithWeightInFile(fact_db &facts, std::string filename, std::string weightfile);
 
+  /// Gets the BC information from a VOG grid.
+  /// @param[in] filename Name of the VOG grid file.
+  /// @param[out] boundary_ids Vector of pairs of BC ID and name.
+  /// @return true if success
   bool readBCfromVOG(std::string filename,
                      std::vector<std::pair<int,std::string> > &boundary_ids) ;
+
+  /// Sets up facts related to grid boundary conditions.
+  /// @param[in,out] facts Fact database to set up grid into.
   void setupBoundaryConditions(fact_db &facts) ;
+
+  /// Creates the `lower`, `upper`, and `boundary_map` facts.
+  /// @param[in,out] facts Fact database to set up grid into.
   void createLowerUpper(fact_db &facts) ;
+
   void createEdgesPar(fact_db& facts) ;
 
 
@@ -194,7 +232,7 @@ namespace Loci {
                    std::vector<entitySet> &local_faces,
                    std::vector<entitySet> &local_cells,
                    store<vector3d<double> > &pos, Map &cl, Map &cr,
-                   multiMap &face2node, 
+                   multiMap &face2node,
                    store<std::string> &boundary_names,
                    store<std::string> &boundary_tags,
                    std::vector<std::pair<std::string,entitySet> > &volTags,
@@ -208,7 +246,7 @@ namespace Loci {
     {
       store<vector3d<double> > pin ;
       pin.setRep(facts.get_fact("pos")) ;
-    
+
       entitySet dom = pin.domain() ;
       pout.allocate(dom) ;
       FORALL(dom,ii) {
@@ -232,7 +270,7 @@ namespace Loci {
   typedef enum {SIMPLE=0,SFC=1,ORB=2,GRAPH=3,RANDOM=4} partitionerSelector ;
   extern partitionerSelector partitionerMethod ;
 
-} 
+}
 
 
 
