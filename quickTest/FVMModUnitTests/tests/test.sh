7c13d20318d1f055e95fb4fa184229304c8d8de2
Anup Zope <anupzope@gmail.com>
2026-05-13 17:08:50 -0500

    Created SERIALRUN variable in quickTest/test.conf.
    This distinguishes serial runs from parallel runs represented
    by MPIRUN variable. Also, removed MPINUMPROC variable since it is
    folded into MPIRUN/SERIALRUN. This is necessary for Slurm-like
    queuing system where the number of resource request is abstracted
    away from the command that spawns the tasks.

--- quickTest/FVMModUnitTests/tests/test.sh
+++ quickTest/FVMModUnitTests/tests/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LOCI_BASE/lib:$LD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=$LOCI_BASE/lib:$DYLD_LIBRARY_PATH
-$MPIRUN -np $MPINUMPROC ../FVMModUnitTest $LOCI_CMD_FLAGS $1 >& $2
+$MPIRUN ../FVMModUnitTest $LOCI_CMD_FLAGS $1 >& $2
 tail -n 1 $2 > $2.tmp
 if ! grep -q "PASSED" $2.tmp ; then echo TEST FVMModUnitTests/$1":" FAILED! >> $2; fi
 rm $2.tmp
